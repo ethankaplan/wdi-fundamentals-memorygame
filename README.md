c802d1fc1569884c9c4e4b9363e9da427536c584
Ethan <N/A>
2019-03-08 17:15:18 -0800

    Added score, reset, shuffle, and no doubleclick

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # wdi-fundamentals-memorygame
+Project by Ethan Kaplan
+SEI - March 2018
+
+Bonuses added:
+> reset button
+> no double click on cards
+> cards shuffle
+> score
--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,9 +22,38 @@ var cards = [{
 
 
 var cardsInPlay = [];
+var score = 0;
+
+
+
+//-----------
+//shuffle is pulled from https://github.com/Daplie/knuth-shuffle
+function shuffle(array) {
+
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+//----------
+
+
 
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
+		document.getElementById("score").textContent = score+=1;
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
@@ -38,13 +67,15 @@ var flipCard = function(){
 	// console.log(cards[cardId].cardImage + " and " + cards[cardId].suit);
 
 	this.setAttribute('src',cards[cardId].cardImage);
-	
+	this.setAttribute('class',"flipped")
+
 	if(cardsInPlay.length===2){
 		checkForMatch();
 	}
 }
 
 var createBoard = function(){
+	shuffle(cards);
 	for(var i = 0;i<cards.length;i++){
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src','images/back.png')
@@ -55,13 +86,22 @@ var createBoard = function(){
 	}
 }
 
-/*var resetButton = function(){
-	cardsInPlay = [];
-	for(var i = 0;i<cards.length;i++){
-		clickedCards[i].setAttribute("src","images/back.png");
-
+var resetButton = function(){
+	var flippedCards = document.getElementsByClassName("flipped");
+	for (var i = 0; i < flippedCards.length; i++){
+		flippedCards[i].setAttribute("src","images/back.png");
+		//flippedCards[i].classList.remove("flipped");
 	}
-}*/
+	for (var i = flippedCards.length-1; i >= 0; i--){
+		flippedCards[i].classList.remove("flipped");
+	}
+
+	shuffle(cards);
+
+	cardsInPlay = [];
+}
+
+
 
 
 
